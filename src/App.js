3ae1c8d2d0f7dd09fc26efe48598bf44894aa635
Rafael Yanagui <rafael.yanagui@gmail.com>
2020-04-16 22:43:14 -0300

    Adiciona função de adicionar e excluir repositorios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("/repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const repository = await api.post("/repositories", {
+      title: `Repositorio ${Date.now()}`,
+      url: "https://nanoincub.com.br",
+      techs: ["NODEJS", "FOCO", "Determinação"],
+    });
+
+    setRepositories([...repositories, repository.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const repository = await api.delete(`/repositories/${id}`);
+
+    if (repository.status === 204) {
+      const repositoryIndex = repositories.findIndex(
+        (repository) => repository.id === id
+      );
+
+      repositories.splice(repositoryIndex, 1);
+
+      setRepositories([...repositories]);
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(({ id, title, url, techs, likes }) => {
+          return (
+            <li key={id}>
+              {title}
+              <button onClick={() => handleRemoveRepository(id)}>
+                Remover
+              </button>
+            </li>
+          );
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
